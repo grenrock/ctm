f6a8b79f53466c65d1d3ca0d32c779d949b285b1
Steven Grenrock <stevengrenrock@live.com>
2023-04-08 11:28:24 -0700

    add text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,12 +47,31 @@ function IndexPage(props: IndexPageProps) {
         variants={contentAnimation(props.isFirstMount)}
         className="space-y-12"
       >
-        <PageTitle
-          pageTitle="Community Team Management"
-          description={null}
-        />
+        <PageTitle pageTitle="Community Team Management" description={null} />
         <AboutParagraph>
-          Stuff.
+          Community Team Management’s approach to HOA financial management is
+          rooted in the belief that open communication and proactive
+          problem-solving are key to a successful partnership. We understand
+          that each community has unique needs and challenges, and we are
+          committed to working closely with the board to tailor our financial
+          management services to meet those needs.
+        </AboutParagraph>
+        <AboutParagraph>
+          We believe in a one-price management fee policy. What this means is
+          that all postage, photocopying, and other incidentals are included in
+          the quoted management fee. You can trust that the quoted fee will not
+          be padded during the year. To keep our costs and your costs low, we
+          work remotely and use electronic means for communication and payments
+          as much as possible, but our top-of-the-line Vantaca and AvidXchange
+          software programs ensure the most secure and comprehensive financial
+          reporting and provide the online payment options, owner account
+          portals, and easy access to HOA documants.
+        </AboutParagraph>
+        <AboutParagraph>
+          Community Team Management LLC is an employee-owned and operated
+          enterprise. Each owner lives in Southern California and has years of
+          HOA financial management experience. We are your neighbors and look
+          forward to having you as part of our Team.
         </AboutParagraph>
       </motion.div>
     </motion.section>
